223ed7f858fa9881fc47f9d5cc58f87f56b19a2a
Jincy7 <jincy713@gmail.com>
2018-12-06 23:26:06 +0900

    Request for Lambda function to search stock by search key.

--- clova/index.js
+++ clova/index.js
@@ -1,6 +1,9 @@
-const uuid = require('uuid').v4
-const _ = require('lodash')
-const {DOMAIN} = require('../config')
+const uuid = require('uuid').v4;
+const _ = require('lodash');
+const request = require('request');
+const {DOMAIN} = require('../config');
+
+const stockSearchUrl = "https://7vudjag5n8.execute-api.ap-northeast-2.amazonaws.com/v1/stocks";
 
 class Directive {
     constructor({namespace, name, payload}) {
@@ -79,16 +82,36 @@ class CEKRequest {
         switch (intent) {
             case 'searchIntent':
                 let searchKey = "";
+                let result = '';
                 cekResponse.appendSpeechText('요청하신 주식을 검색할게요');
                 console.log(slots.valueOf()); // 예시 { StockNameSlot: { name: 'StockNameSlot', value: '네이버 주식' } }
                 const searchKeySlot = slots.StockNameSlot;
                 if (slots.length != 0 && searchKeySlot) {
                     searchKey = searchKeySlot.value;
+                    request.post({
+                        url: stockSearchUrl,
+                        form: JSON.stringify({stockID: "테스트텥"})
+                    }, function callback(err, httpResponse, body) {
+                        if (err) {
+                            return console.error('Request failed:', err);
+                        }
+
+                        console.log('Request Success! Server responded with:', JSON.parse(body).stockID);
+                        result = JSON.parse(body).stockID;
+                    });
+                    cekResponse.appendSpeechText('요청하신 주식은 ' + result);
+                } else {
+                    // 슬롯에 아무것도 없는 경우이므로 multiturn 응답을 통해 사용자에게 다시 회사명을 말해달라고 요청
+                    // cekResponse.setSimpleSpeechText('죄송해요, 회사를 찾지 못했어요. 앞으로 서비스해 드리기 위해 회사명을 다시 한번만 말해주세요.')
+                    // cekResponse.setMultiturn({
+                    //  intent: 'AddComapanyIntent',
+                    // });
                 }
                 console.log(searchKey);
                 /* TODO dev Blueprint
                 const stockResult = searchOnWeb(searchKey); 받은 주식값으로 검색 함수에 넣어서 값 반환 받기
-                결과 나오면 appendSpeechText 로 아래 resultText 처럼 함수 하나 만들어서
+                결과 나오면 appendSpeechText 로 아래 resultText 처럼 함수 하나 만들어서 클로바로 출력하고
+                다이나모 디비에 저장!
 */
                 break;
             case 'ThrowDiceIntent':
